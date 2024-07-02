298c34b6ba99db2ce6fcb39a17f0c6b589156b62
vandana <87233177+engrvandana@users.noreply.github.com>
2024-07-02 23:18:05 +0500

    Update my_application.py

--- my_application.py
+++ my_application.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello, World! This is my Flask application deployed on AWS Elastic Beanstalk."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
